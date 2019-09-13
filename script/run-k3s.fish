8eb86895e261adeac7dde40df6f8cb081153471c
shinyay <shinya.com@gmail.com>
2019-09-13 14:07:19 +0900

    Add a script for run k3s as docker compose

---
+++ script/run-k3s.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/env fish
+
+function run_k3s
+  argparse -n run_k3s 'h/help' 'f/file' -- $argv
+  or return 1
+
+  if set -lq _flag_help
+    echo "run_k3s.fish -f <DOCKER-COMPOSE-YAML>"
+    return
+  end
+
+  set -lq _flag_file
+  or set -l _flag_file ../docker-compose.yml
+
+  docker-compose -f $_flag_file up -d
+end
+
+run_k3s $argv
